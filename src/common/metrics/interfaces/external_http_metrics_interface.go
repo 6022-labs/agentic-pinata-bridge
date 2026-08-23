bfce25a351e4686c57a573ab94f89ae33feb7674
Pierre Fourny <pierre.fourny76@gmail.com>
2026-08-23 11:46:55 +0200

    lint: fix format

--- src/common/metrics/interfaces/external_http_metrics_interface.go
+++ src/common/metrics/interfaces/external_http_metrics_interface.go
@@ -7,6 +7,12 @@ import (
 
 // ExternalHttpMetricsInterface records http.client.* metrics for outbound calls.
 type ExternalHttpMetricsInterface interface {
-	RecordRequest(ctx context.Context, method, serverUrl string, statusCode int, duration time.Duration, responseSize int64)
+	RecordRequest(
+		ctx context.Context,
+		method, serverUrl string,
+		statusCode int,
+		duration time.Duration,
+		responseSize int64,
+	)
 	RecordTransportError(ctx context.Context, method, serverUrl string, duration time.Duration, err error)
 }
